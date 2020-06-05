62894124490b58ca02fcdb75cefd025b1f4c59f1
xosemanolo <xose.manolo.rodriguez@gmail.com>
2020-06-05 17:45:11 -0400

    Create redux store

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,6 @@
 import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 // import axios from 'axios';
 
 const Register = () => {
@@ -97,7 +99,7 @@ const Register = () => {
           <input type='submit' className='btn btn-primary' value='Register' />
         </form>
         <p className='my-1'>
-          Already have an account? <a href='login.html'>Sign In</a>
+          Already have an account? <Link to='/register'>Sign In</Link>
         </p>
       </section>
     </Fragment>
